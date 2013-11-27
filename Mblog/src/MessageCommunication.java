e46caa06a43bc945d64338d939226a53a9753f97
victor <victor@victor-VirtualBox.(none)>
2013-11-26 16:24:12 -0800

    apply Vivek's changes

---
+++ Mblog/src/MessageCommunication.java
@@ -0,0 +1,24 @@
+
+public class MessageCommunication {
+
+	public MessageCommunication() {
+		
+	}
+	
+	public void sendPrepareMsg(int paxosId, BallotPair proposedBallotNumPair) {
+		//BroadCast this message to everybody.
+		//only broadcast a string with a delimtetr
+	}
+	
+	public void sendAckToPrepare(int paxosId, BallotPair ownBallotNumPair, BallotPair acceptedBallotNumPair, String acceptedValue) {
+		
+	}
+	
+	public void sendAccept(int paxosId, BallotPair proposeBallotNumPair, String ValueToWrite) {
+		
+	}
+
+	public void sendDecide(int paxosId, String acceptedValue) {
+		//keep sending it periodically Ask_Victor would it block the call
+	}
+}
--- Mblog/src/Paxos.java
+++ Mblog/src/Paxos.java
@@ -1,23 +1,192 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Paxos {
 
 	int id; // Monotonically increasing in each server, and represents log entry ID
-	BallotPair acceptedBallotPair; //highest accepted ballot number for this paxos instance
+	int replicaId;
+	int numPrepareAck;
+	BallotPair acceptedBallotNumPair; //highest accepted ballot number for this paxos instance
 	String acceptedValue; // last accepted (decided) Value for this paxos instance, once set, could not be changed
-	BallotPair ballotNum;
-	
-	public Paxos (int id)
+	BallotPair proposeBallotNumPair; // for proposing and increasing on loosing
+	BallotPair ownBallotNumPair; // for responding to prepare messages
+	MessageCommunication Msg;
+	int numActiveServers;
+	String ValueToWrite;
+	ArrayList<AcceptedBallotNumAndValue> ListAcceptedNumAndValue;
+	ArrayList<AcceptedBallotNumAndValue> ListAcceptMsgsAndCounter;
+	int acceptableFailures;
+	int receivedAccepts;
+	Logging logger;
+	public Paxos (int id, int replicaid, int num_active_servers, String write_req_from_client, Logging replicaLogger)
 	{
-		this.acceptedBallotPair = new BallotPair(0, 0);
-		acceptedValue = "";
-		ballotNum = new BallotPair(0, 0);
+		this.id = id;
+		this.numActiveServers = num_active_servers;
+		this.numPrepareAck = 0;
+		this.acceptedBallotNumPair = new BallotPair();
+		this.ownBallotNumPair = new BallotPair();
+		this.acceptedValue = "";
+		this.proposeBallotNumPair = new BallotPair(0,replicaId);
+		this.Msg = new MessageCommunication();
+		this.ListAcceptedNumAndValue = new ArrayList<AcceptedBallotNumAndValue>();
+		this.ListAcceptMsgsAndCounter = new ArrayList<AcceptedBallotNumAndValue>();
+		this.ValueToWrite = write_req_from_client;
+		this.acceptableFailures = 1; //Ask_Victor for t
+		this.receivedAccepts = 0;
+		this.logger = replicaLogger; //logger of the replica instantiated in replica class - used by paxos instances.
+		this.logger.write("Paxos Id:" + String.valueOf(this.id) + " created.");
+		
 	}
+	
+	/**
 	public Paxos (String logLine)
 	{
 		String logParts [] = logLine.split(" ");
 		this.acceptedBallotPair = new BallotPair(Integer.parseInt(logParts[0]),Integer.parseInt(logParts[1]));
 		this.acceptedValue = logParts[2];
 		this.ballotNum = new BallotPair(Integer.parseInt(logParts[0]),Integer.parseInt(logParts[1]));
+	}
+	*/
+	
+	//write prepare
+	//ack to prepare
+	//write accept
+	
+	/** sendPrepare
+	 *  Called - on receiving "post" request
+	 *  Does - becomes a leader and sends prepare msg to all
+	 */
+	public void sendPrepare()
+	{
+	    //do nack - if not enough nacks - then propose old ballotnumber
+		//maintain - acknum with proposeballotnum in arraylist or hashmap
+		boolean keepTrying = true;
+		//Keep sending prepare message until you hear back from majority
+		//change activeServers to totalServers
+		while(keepTrying) {
+			if(this.numPrepareAck > this.numActiveServers/2)
+				keepTrying = false;
+			else {
+				try {
+					//Increase own proposeBallotNumPair
+					this.proposeBallotNumPair.ballotNum = this.proposeBallotNumPair.ballotNum + 1;
+					this.numPrepareAck = 0; // Ask_Victor
+					this.logger.write("PaxosID:" + String.valueOf(this.id) + " SENT PREPARE BALLOTNUMBER" + this.proposeBallotNumPair.toString());
+					Msg.sendPrepareMsg(this.id, this.proposeBallotNumPair);
+					//Waiting for people to respond within 150 milliseconds otherwise - Increase BallotNumber and Propose again. - Ask_Victor
+					Thread.sleep((long) (Math.random()*150));
+				}
+				catch(InterruptedException e) {
+					e.printStackTrace();
+				}		
+			}
+		}
+	}
+	
+	/** onreceivePrepare
+	 * Called - when a replica - receives a prepare msg and instantiates a new paxos instance if not spawned already.
+	 * Does - responds back to the leader if ballot number proposed is higher than or equal to the earlier accepted ballot number
+	 * 
+	 */
+	public void onreceivePrepare(BallotPair proposedBallotNumPair) {
+		//check if this is activePaxos or already decided Paxos  Ask_Victor
+		
+		this.logger.write("PaxosID:" + String.valueOf(this.id) + " RECEIVED PREPARE MSG WITH BALLOTNUMBER " + proposedBallotNumPair.toString());
+		if(proposedBallotNumPair.compareTo(this.ownBallotNumPair) >= 0) {
+			this.ownBallotNumPair = proposedBallotNumPair;
+			Msg.sendAckToPrepare(this.id, this.ownBallotNumPair, this.acceptedBallotNumPair, this.acceptedValue);
+			this.logger.write("PaxosID:" + String.valueOf(this.id) + " SENT ACK TO PREPARE MSG WITH BALLOTNUMBER " + proposedBallotNumPair.toString());
+		}
+		//should we send NACK as well Ask_Victor
+	}
+	
+	/** onreceiveAckToPrepare
+	 * Called - when a replica - receives an "AckToPrepare" msg from another replica for ongoing paxos
+	 * then replica picks up corresponding paxos and calls this method
+	 * r sends for replica
+	 * Does - increases the numPrepareAck and 
+	 */
+	public void onreceiveAckToPrepare(BallotPair r_ownBallotNumPair, BallotPair r_acceptedBallotNumPair, String r_acceptedValue) {
+		//I assume that the received ack is for current ballotnum pair which I proposed most recently and increase ack for it.
+		//Ask_Victor
+		this.logger.write("PaxosID:" + String.valueOf(this.id) + " RECEIVED ACK TO PREPARE MSG WITH BALLOTNUMBER " + r_ownBallotNumPair.toString());
+		if(this.proposeBallotNumPair.compareTo(r_ownBallotNumPair) == 0) {
+			this.numPrepareAck = this.numPrepareAck + 1;
+		    this.ListAcceptedNumAndValue.add(new AcceptedBallotNumAndValue(r_acceptedBallotNumPair, r_acceptedValue));
+		    //On receiving ack from majority
+		    if(this.numPrepareAck > this.numActiveServers/2) {
+		    	Iterator<AcceptedBallotNumAndValue> itr = this.ListAcceptedNumAndValue.iterator();
+		    	boolean novalue = true;
+		    	AcceptedBallotNumAndValue highestBallotNumObj = new AcceptedBallotNumAndValue(new BallotPair(0,0),"");
+		    	while(itr.hasNext()) {
+		    		AcceptedBallotNumAndValue testObj = itr.next();
+		    		if(testObj.r_acceptedValue != "")
+		    			novalue = false;
+		    		if(testObj.r_acceptedBallotNumPair.compareTo(highestBallotNumObj.r_acceptedBallotNumPair) > 0) {
+		    			highestBallotNumObj = testObj;
+		    		}
+		    	}
+		    	if(novalue) {
+		    		this.logger.write("PaxosID:" + String.valueOf(this.id) + " RECVD ACK TO PREPARE FROM MAJORITY, SENDING ACCEPT MSG WITH VALUE " + this.ValueToWrite);
+		    		Msg.sendAccept(this.id, this.proposeBallotNumPair,this.ValueToWrite);
+		    	}
+		    	else {
+		    		this.logger.write("PaxosID:" + String.valueOf(this.id) + " RECVD ACK TO PREPARE FROM MAJORITY, SENDING ACCEPT MSG WITH VALUE " + highestBallotNumObj.r_acceptedValue);
+		    		Msg.sendAccept(this.id, this.proposeBallotNumPair, highestBallotNumObj.r_acceptedValue);
+		    	}
+		    }
+		}
+		else {
+			//Log a message - this case has to be discussed with Victor
+		}
+		
+	}
+	
+	/** onreceiveAccept
+	 * 
+	 */
+	public void onreceiveAccept(BallotPair ballotNumPair, String value) {
+		this.logger.write("PaxosID:" + this.id + "RECVD ACCEPT MSG WITH BALLOTNUMBER " + ballotNumPair.toString() + " AND VALUE " + value); 
+		AcceptedBallotNumAndValue tempObj = new AcceptedBallotNumAndValue(ballotNumPair,value);
+		Iterator<AcceptedBallotNumAndValue> itr = this.ListAcceptMsgsAndCounter.iterator();
+		boolean increasedCount = false;
+		while(itr.hasNext()) {
+			AcceptedBallotNumAndValue itrObj = itr.next();
+			if(itrObj.equalTo(tempObj)) {
+				itrObj.numTimes = itrObj.numTimes + 1;
+				increasedCount = true;
+			}
+		}
+		if(!increasedCount) {
+			this.ListAcceptMsgsAndCounter.add(tempObj);
+		}
+		
+		itr = this.ListAcceptMsgsAndCounter.iterator();
+		while(itr.hasNext()) {
+			AcceptedBallotNumAndValue itrObj = itr.next();
+			if(itrObj.numTimes >= this.numActiveServers - this.acceptableFailures) {
+				this.logger.write("PaxosID:" + this.id + "RECVD ACCEPT MSG FROM ALL - ACCEPTABLE FAILURES WITH VALUE " + value);
+				//Periodically send
+				Msg.sendDecide(this.id,itrObj.r_acceptedValue); //broadcast it to all
+				//decide to yourself
+				this.onreceiveDecide(itrObj.r_acceptedValue);
+			}
+		}
+		
+		//ask about deciding - do we go for all new values
+		if(ballotNumPair.compareTo(this.ownBallotNumPair) >= 0) {
+			this.acceptedBallotNumPair = ballotNumPair;
+			this.acceptedValue = value;
+			Msg.sendAccept(this.id,ballotNumPair, value); //broadcast it to all
+		}
+	}
+	
+	/** onreceiveDecide
+	 * 
+	 */
+	public void onreceiveDecide(String value) {
+		this.logger.write("PaxosID:" + this.id + "RECVD DECIDE MSG WITH VALUE " + value);
+		//WritetoLog where logindex = this.paxosId and value = parameter
 	}
 	
 }
